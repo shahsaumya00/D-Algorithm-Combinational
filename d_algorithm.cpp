1b2cc5164f4ba0a1c07eecf6717f52e42cefa19a
Saumya Shah <31558317+shahsaumya00@users.noreply.github.com>
2020-11-25 10:36:51 +0530

    Add files via upload

--- d_algorithm.cpp
+++ d_algorithm.cpp
@@ -162,15 +162,12 @@ void printGraph(vector<pair<int,int> > adj[], int V)
     int v, w; 
     for (int u = 0; u < V; u++) 
     { 
-        cout << "gate " << u << " leads to gate "; 
         for (auto it = adj[u].begin(); it!=adj[u].end(); it++) 
         { 
             v = it->first; 
             w = it->second; 
-            cout << "\tNode " << v << " via wire"
-                 << w << " "; 
+
         } 
-        cout << "\n"; 
     } 
 } 
 
@@ -181,9 +178,9 @@ void print_tc(vector<vector<char>> tc)
     {
         for (int j = 0; j < tc[i].size(); j++)
         {
-            cout << tc[i][j] << " ";
+//            cout << tc[i][j] << " ";
         }    
-        cout << endl;
+//        cout << endl;
     }
 }
 
@@ -295,24 +292,18 @@ vector <pair<int, int> > jfunct(vector<vector<char>> tc, int gate_type[], int ga
     return jfrontier;
 }
 
-// justify input of gates which are in J-frontier
 vector<char> justify(vector<vector<char>> tmp_tc,char dcubes[8][12][3],int gate_type[], int gatedata[][6]) {
+    // Get J frontier
     vector <pair<int, int> > jfrontier;
-//    int justify_call = func_call;
     int g2,e2;
     jfrontier = jfunct(tmp_tc, gate_type, gatedata);
-//    cout << "jfrontier" << ": ";
-    for(auto i=jfrontier.begin();i<jfrontier.end();i++) {
-        cout << i->first << " ";
-    }
-    cout << "" << endl;
     if(jfrontier.empty()) {
-        cout << "jfrontier = empty" << endl;
         return tmp_tc[tmp_tc.size()-1];
     }
     for (auto it = jfrontier.begin(); it!=jfrontier.end(); it++) {
         g2 = it->first;
         e2 = it->second;
+    //Find suitable Singular covers
         vector<vector<char>> tmp_sc;
         for(int i=0; i<4; i++) {
             vector<char> vec_sc;
@@ -331,9 +322,8 @@ vector<char> justify(vector<vector<char>> tmp_tc,char dcubes[8][12][3],int gate_
         }
         vector<vector<char>> tmp2_tc;
         vector<char> tmp3_tc;
-
+    // check whether assigned values are consistent or not
         while(!tmp_sc.empty()) {
-            cout << "in while - justify" << endl;
             tmp_tc[tmp_tc.size()-1][gatedata[e2][2]] = tmp_sc[tmp_sc.size()-1][0];
             tmp_tc[tmp_tc.size()-1][gatedata[e2][3]] = tmp_sc[tmp_sc.size()-1][1];
             tmp_tc[tmp_tc.size()-1][e2] = tmp_sc[tmp_sc.size()-1][2];
@@ -346,9 +336,7 @@ vector<char> justify(vector<vector<char>> tmp_tc,char dcubes[8][12][3],int gate_
                     tmp2_tc[tmp_tc.size()-1][e] = 'x';
                 }                
             } 
-            cout << "calling justify from justify" << endl;
             tmp3_tc = justify(tmp2_tc,dcubes,gate_type,gatedata);
-            cout << "returning from justify to justify" << endl;
             if(tmp3_tc.size()==1) {
                 if(tmp_tc.empty()) {
                     return {'0'};
@@ -376,76 +364,49 @@ vector<char> dalgo(vector<vector<char>> tc, int num_out, int gate_type[],char dc
 // function to run D algo once branch detected
 vector<char> D_algo(vector<vector<char>> tc, int num_out, int gate_type[],char dcubes[8][12][3],int gatedata[][6], vector<vector<int>> branchdata) {        
     vector <pair<int, int> > dfrontier = dfunct(tc,gate_type,gatedata);
-    cout << "dfrontier" <<  " ";
-    cout << dfrontier.size() << endl;
 
-    //Base condition
+    //Base condition: If dfrontier is empty and fault has reached PO, return tc. If dfrontier is empty and fault hasn't reached PO, return 0 which indicates that fault is untestable.
     int flag = 0;
     if(dfrontier.empty()){
-        cout << "in base condition" << endl;
         for(int i = tc[tc.size()-1].size() - num_out; i < tc[tc.size()-1].size(); i++) {
-            cout << i << " " << tc[tc.size()-1][i] << endl;
             if (tc[tc.size()-1][i] == 'D' || tc[tc.size()-1][i] == 'E') {
                 flag = 1;
             }
         }
-        cout << flag << endl;
         if(!flag){
-            cout << "returning 0  --1" << endl;
             return {'0'};
         }           
         else {
-            cout << "returning tc  --2" << endl;
             return tc[tc.size()-1]; 
         }
                  
     }
-    //Pick 1 gate  in dfrontier
-    //DEFINE TMP_TC
 
     vector<vector<char>> dtmp_tc;
     dtmp_tc.push_back(tc[tc.size()-1]);
     int g1,e1;
     int flag1 = 0;
     for (auto it = dfrontier.begin(); it!=dfrontier.end(); it++) { 
-        cout << "inside loop:dfrontier" << endl;
+        //PDC
         vector<vector<char>> tmp_pdc; 
         g1 = it->first;
         e1 = it->second; 
-//        d_path.push_back(e1);
-        cout << "push_back: d_path" << e1 << endl;
-        cout << g1 << endl;
         for(int i=8; i<12; i++) {
             if(dcubes[gate_type[g1]][i][0] == dtmp_tc[dtmp_tc.size()-1][e1]) {
                 tmp_pdc.push_back({dcubes[gate_type[g1]][i][0],dcubes[gate_type[g1]][i][1],dcubes[gate_type[g1]][i][2]});
             }
         }
         for(int i = 0; i< tmp_pdc.size();i++) {
-            for(int j=0;j<3;j++) {
-                cout << tmp_pdc[i][j] << " ";
-            }
-            cout << "" << endl;
+
         }
-        cout << "stored tmp_pdc" << endl;
         while(!tmp_pdc.empty()) {
-            cout << "in while" << endl;
             dtmp_tc[dtmp_tc.size()-1][e1] = tmp_pdc[tmp_pdc.size()-1][0];
-
-            cout << "before" << gatedata[e1][5] << " " << dtmp_tc[dtmp_tc.size()-1][gatedata[e1][5]] << " " << tmp_pdc[tmp_pdc.size()-1][1] << endl;
-
             dtmp_tc[dtmp_tc.size()-1][gatedata[e1][5]] = tmp_pdc[tmp_pdc.size()-1][1];
-
-            cout << "after" << gatedata[e1][5] << " " << dtmp_tc[dtmp_tc.size()-1][gatedata[e1][5]] << " " << tmp_pdc[tmp_pdc.size()-1][1] << endl;
-
             dtmp_tc[dtmp_tc.size()-1][gatedata[e1][4]] = tmp_pdc[tmp_pdc.size()-1][2];
-
             tmp_pdc.pop_back();
-
-            cout << "calling justify from dalgo" << endl;
-            print_tc(dtmp_tc);
+        // Calling Justify
             vector<char> out_tmp; 
             out_tmp= justify(dtmp_tc,dcubes,gate_type,gatedata);
-            cout << "returning to dalgo from justify" << endl;
 
             if(out_tmp.size()!=1) {
                 dtmp_tc.push_back(out_tmp);
@@ -453,27 +414,20 @@ vector<char> D_algo(vector<vector<char>> tc, int num_out, int gate_type[],char d
                 break;
             }
             else {
-//                cout << "pop_back: d_path" << d_path[d_path.size()-1] << endl;
-//                d_path.pop_back();
             }
         }
         if(flag1)
             break;
     }
     if(flag1) {
-        cout << "calling dalgo from D_algo" << endl;
         tc[tc.size()-1] = dalgo(dtmp_tc,num_out,gate_type,dcubes,gatedata,branchdata);
-        cout << "returning from dalgo to D_algo" << endl;
         tc.push_back(tc[tc.size()-1]);
-        cout << "returing tc --3" << endl;
         return tc[tc.size()-1];
         
     }
     else {
-        cout << "returning 0 --4" << endl;
         return {'0'};
     }
-cout << "returning 0 -extra" << endl;
 return {'0'};
 }
 // function for D algo
@@ -486,7 +440,6 @@ vector<char> dalgo(vector<vector<char>> tc, int num_out, int gate_type[],char dc
         if(tc[tc.size()-1][i] == 'D' || tc[tc.size()-1][i] == 'E') {
             if(gate_type[gatedata[i][1]] == 7) {
                 for(int j=0;j<branchdata.size();j++) {
-                    cout << branchdata[j][0] << endl;
                     if(branchdata[j][0]==i) {
                         int all_x = 1;
                         for(int k=1;k<b_data.size();k++) {
@@ -494,7 +447,6 @@ vector<char> dalgo(vector<vector<char>> tc, int num_out, int gate_type[],char dc
                                 all_x = 0;
                             }
                         } 
-                        cout << "" << endl; 
                         if(all_x) {
                             branch = 1;
                             b_data = branchdata[j];
@@ -515,14 +467,8 @@ vector<char> dalgo(vector<vector<char>> tc, int num_out, int gate_type[],char dc
         while(!b_data.empty()) {
             tc.push_back(tc[tc.size()-1]);
             tc[tc.size()-1][b_data[b_data.size()-1]] = assgn;
-//            cout << "push_back: d_path" << b_data[b_data.size()-1] << endl; 
-//            d_path.push_back(b_data[b_data.size()-1]);
             b_data.pop_back();
-            cout << "tc-branch" << endl;
-            cout << "calling D_algo from dalgo-branch" << endl;
             out_final = D_algo(tc,num_out,gate_type,dcubes,gatedata,branchdata);
-            cout << "returning from D_algo to dalgo-branch" << endl;
-            cout << out_final.size() << endl;
             if(out_final.size()!=1) {
                 flag2 = 1;
                 tc.push_back(out_final);
@@ -530,34 +476,24 @@ vector<char> dalgo(vector<vector<char>> tc, int num_out, int gate_type[],char dc
             }
             else {
                 tc.pop_back();
-//                cout << "pop_back: d_path" << d_path[d_path.size()-1] << endl;
-//                d_path.pop_back();
             }
-            //call function
         }
         if(!flag2) {
-            cout << "returning 0 --5" << endl;
             return {'0'};
         }
             
     }
     else {
-//        call function
 
-        cout << "calling D_algo from dalgo-nonbranch" << endl;
         out_final = D_algo(tc,num_out,gate_type,dcubes,gatedata,branchdata);
-        cout << "returning from D_algo to dalgo-nonbranch" << endl;
         if(out_final.size()==1) {
-            cout << "returning 0  --6" << endl;
             return {'0'};
         }
         else {
-            cout << "returning tc --7" << endl;
             tc.push_back(out_final);
             return tc[tc.size()-1];
         }
     }   
-cout << "returning 0  --8" << endl;
 return {'0'};
 }
 
@@ -565,68 +501,131 @@ int main() {
 
     // Input
     // Graph
-    int V,edge;
-    cout << "Enter the Number of Vertices: ";
-    cin >> V;
-    cout << "Enter the Number of Edges: ";
-    cin >> edge;
-    vector<pair<int, int> > adj[V];
 
+    int V;
+    int edge;
     int u,n,w;
-    cout << "Enter u v w:";
-    for(int i=0; i<V; i++) {
-        cin >> u >> n >> w;
-        addEdge(adj,u,n,w);
+    int var;
+    cin >> var;
+    
+   
+    vector <vector<int>> gate_type_tmp;
+    if(var == 2) {
+        V = 16;
+        edge = 16;
     }
-    printGraph(adj, V);
+    else if(var ==1) {
+        V = 8;
+        edge = 8;
+    }
+    else if(var == 3) {
+        V = 16;
+        edge = 16;
+    }
+    else if(var ==4) {
+        V = 8;
+        edge = 8;
+    }
+    else if(var ==5) {
+        V = 16;
+        edge = 16;
+    }
+    vector<pair<int, int> > adj[V];  
+    int gate_type[V];     
+    if(var == 2) {      
+        addEdge(adj, 0, 8, 20); 
+        addEdge(adj, 1, 6, 21); 
+        addEdge(adj, 2, 9, 22); 
+        addEdge(adj, 3, 11, 23); 
+        addEdge(adj, 4, 11, 24); 
+        addEdge(adj, 5, 13, 25); 
+        addEdge(adj, 6, 8, 26);
+        addEdge(adj, 6, 9, 27); 
+        addEdge(adj, 7, 12, 31); 
+        addEdge(adj, 7, 13, 32); 
+        addEdge(adj, 8, 10, 28); 
+        addEdge(adj, 9, 10, 29); 
+        addEdge(adj, 10, 7, 30);
+        addEdge(adj, 11, 12, 33);
+        addEdge(adj, 12, 14, 34);
+        addEdge(adj, 13, 15, 35);
+        printGraph(adj, V);
+        gate_type_tmp.push_back({-1,-1,-1,-1,-1,-1,7,7,0,1,2,2,0,0,-2,-2});  
+    }
+    else if(var ==1) {
+        addEdge(adj, 0, 4, 20); 
+        addEdge(adj, 1, 3, 21); 
+        addEdge(adj, 2, 5, 22); 
+        addEdge(adj, 3, 4, 23); 
+        addEdge(adj, 3, 5, 24); 
+        addEdge(adj, 4, 6, 25); 
+        addEdge(adj, 5, 6, 26);
+        addEdge(adj, 6, 7, 27);
+        printGraph(adj, V); 
+        gate_type_tmp.push_back({-1,-1,-1,7,0,1,3,-2});
+    }
+    else if(var == 3) {      
+        addEdge(adj, 0, 8, 20); 
+        addEdge(adj, 1, 6, 21); 
+        addEdge(adj, 2, 9, 22); 
+        addEdge(adj, 3, 11, 23); 
+        addEdge(adj, 4, 11, 24); 
+        addEdge(adj, 5, 13, 25); 
+        addEdge(adj, 6, 8, 26);
+        addEdge(adj, 6, 9, 27); 
+        addEdge(adj, 7, 12, 31); 
+        addEdge(adj, 7, 13, 32); 
+        addEdge(adj, 8, 10, 28); 
+        addEdge(adj, 9, 10, 29); 
+        addEdge(adj, 10, 7, 30);
+        addEdge(adj, 11, 12, 33);
+        addEdge(adj, 12, 14, 34);
+        addEdge(adj, 13, 15, 35);
+        printGraph(adj, V);
+        gate_type_tmp.push_back({-1,-1,-1,-1,-1,-1,7,7,0,1,4,2,0,0,-2,-2});  
+    }
+    else if(var == 4) {
+        addEdge(adj, 0, 4, 20); 
+        addEdge(adj, 1, 3, 21); 
+        addEdge(adj, 2, 5, 22); 
+        addEdge(adj, 3, 4, 23); 
+        addEdge(adj, 3, 5, 24); 
+        addEdge(adj, 4, 6, 25); 
+        addEdge(adj, 5, 6, 26);
+        addEdge(adj, 6, 7, 27);
+        printGraph(adj, V); 
+        gate_type_tmp.push_back({-1,-1,-1,7,5,1,3,-2});
+    }
+    else if(var == 5) {      
+        addEdge(adj, 0, 8, 20); 
+        addEdge(adj, 1, 6, 21); 
+        addEdge(adj, 2, 9, 22); 
+        addEdge(adj, 3, 11, 23); 
+        addEdge(adj, 4, 11, 24); 
+        addEdge(adj, 5, 13, 25); 
+        addEdge(adj, 6, 8, 26);
+        addEdge(adj, 6, 9, 27); 
+        addEdge(adj, 7, 12, 31); 
+        addEdge(adj, 7, 13, 32); 
+        addEdge(adj, 8, 10, 28); 
+        addEdge(adj, 9, 10, 29); 
+        addEdge(adj, 10, 7, 30);
+        addEdge(adj, 11, 12, 33);
+        addEdge(adj, 12, 14, 34);
+        addEdge(adj, 13, 15, 35);
+        printGraph(adj, V);
+        gate_type_tmp.push_back({-1,-1,-1,-1,-1,-1,7,7,0,2,4,2,5,1,-2,-2});  
+    }
 
-//    int V = 17;
-//    int edge = 17;
- 
-/*  addEdge(adj, 2, 7, 22); 
-    addEdge(adj, 1, 9, 21); 
-    addEdge(adj, 7, 9, 29); 
-    addEdge(adj, 3, 10, 23); 
-    addEdge(adj, 9, 12, 34);
-    addEdge(adj, 4, 11, 24); 
-    addEdge(adj, 5, 11, 25);
-    addEdge(adj, 6, 14, 26); 
-    addEdge(adj, 7, 10, 28);
-    addEdge(adj, 10, 12, 27); 
-    addEdge(adj, 8, 13, 32); 
-    addEdge(adj, 11, 13, 31);    
-    addEdge(adj, 8, 14, 33); 
-    addEdge(adj, 13, 15, 35);
-    addEdge(adj, 14, 16, 36); 
-    addEdge(adj, 12, 8, 30);  */
-
-/*  2, 7, 22 
-    1, 9, 21 
-    7, 9, 29 
-    3, 10, 23 
-    9, 12, 34);
-    addEdge(adj, 4, 11, 24); 
-    addEdge(adj, 5, 11, 25);
-    addEdge(adj, 6, 14, 26); 
-    addEdge(adj, 7, 10, 28);
-    addEdge(adj, 10, 12, 27); 
-    addEdge(adj, 8, 13, 32); 
-    addEdge(adj, 11, 13, 31);    
-    addEdge(adj, 8, 14, 33); 
-    addEdge(adj, 13, 15, 35);
-    addEdge(adj, 14, 16, 36); 
-    addEdge(adj, 12, 8, 30);  */
-
-
-    int gate_type[V];
-    int gatetype;
-    cout << "Enter gate type:";
     for(int i=0;i<V;i++) {
-        cin >> gatetype;
-        gate_type[i] = gatetype;
+        if(!gate_type_tmp.empty())
+            gate_type[i] = gate_type_tmp[gate_type_tmp.size()-1][i];
     }
+    
+    
 
-//    int gate_type[V] = {-1,-1,-1,-1,-1,-1,-1,7,7,0,1,2,2,0,0,-2,-2};  
+
+      
 //    int fault_net = 6;
 //    char fault = 'D';
 //============================================================================
@@ -652,7 +651,7 @@ int main() {
 
     for(int j=0;j<2;j++) { 
         for (int i=0;i<edge;i++) {
-            cout << faults[j] << " Fault at line " << i << endl;
+            cout << "s-a-" << j << " Fault at w" << i +20 << endl;
             fault = faults[j];
             fault_net = i;
             //Initialization
@@ -665,9 +664,8 @@ int main() {
                 tc.push_back(v);
             }
             tc.push_back(tc[tc.size()-1]);
-            cout << endl;
+            
             // Fault assignment
-            // Get gate numbers for pdcf(0) and pdc(1)
             int gatedata[edge][6];
             for (int i=0;i<edge;i++) {
                 for(int j=0;j<6;j++) {
@@ -682,13 +680,6 @@ int main() {
                 }
             } 
 
-            for(int i=0;i<edge;i++) {
-                cout << i << ": ";
-                for(int j=0;j<6;j++) {
-                    cout  << gatedata[i][j] << " ";
-                }
-                cout << "" << endl;
-            }
 
             vector<vector<int>> branchdata;
             for(int e=0; e<edge; e++) {
@@ -696,15 +687,6 @@ int main() {
                     branchdata.push_back(getbranchdata(adj, V, e,gatedata[e][1]));
                 }
             }
-
-            for(int i=0;i<branchdata.size();i++) {
-                    for (int j=0;j<branchdata[i].size();j++) {
-                    cout << branchdata[i][j] << " ";
-                }
-                cout << "" << endl;
-            }
-
-            cout << "" << endl;
             tc[tc.size()-1][fault_net] = fault;
             tc.push_back(tc[tc.size()-1]);
 
@@ -717,8 +699,6 @@ int main() {
                 }
             }
             tc.push_back(tc[tc.size()-1]);
-            print_tc(tc);
-            cout << "============" << endl;
 
             // D algo 
             int num_out = 0;   
@@ -728,51 +708,47 @@ int main() {
                 }
             }
             int num_in = 0;
-            cout << "num input" << " ";
             for(int j =0; j<V;j++) {
                 if(gate_type[j]==-1) {
-                    cout << j << " ";
                     num_in++;
                 }
             }
-            cout << num_in << endl;
 
             vector<char> out(edge);
-            cout << "calling dalgo from main" << endl;
             out = dalgo(tc,num_out,gate_type,dcubes,gatedata,branchdata);
-            cout << "returning from dalgo to main" << endl;
             if (out.size()==1) {
                 cout << "FAULT UNTESTABLE" << endl;
             }
             else {
                 
                 tc[tc.size()-1] = out;
-                print_tc(tc);
                 cout << "TEST COMPLETED" << endl;
                 cout << "Test Vector" << ": ";
                 for(int i=0;i<num_in;i++) {
-                    cout << tc[tc.size()-1][i] << " ";
+                    if(tc[tc.size()-1][i] == 'D') {
+                       cout << 1 << " "; 
+                    }                        
+                    else if(tc[tc.size()-1][i] == 'E') {
+                        cout << 0 << " ";
+                    }
+                    else {
+                        cout << tc[tc.size()-1][i] << " ";
+                    }
+                    
                 }
                 cout << "" << endl; 
                 cout << "PATH (EdgeNumber-[GateNumber]): ";
                 for(int i=0;i<tc[tc.size()-1].size();i++) {
                     if(tc[tc.size()-1][i]=='D' || tc[tc.size()-1][i]=='E') {
-                        cout << i << "-[" << gatedata[i][1] << "]-";
+                        cout << i + 20 << "-[" << gatedata[i][1] << "]-";
                     }
                 }
             }
 
-            /*    for(int i=0;i<d_path.size();i++) {
-                cout << d_path[i] << "[" << gatedata[d_path[i]][1] << "]" << " " ;
-            }
-            cout << "" << endl;*/
+           
+            cout << "" << endl;                
 
 
-
-            cout << "" << endl;
-        
-                
-        
         }   
     }
 
